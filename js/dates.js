8f0232bab3fce5fd9cc0fe515b623b1d8908864f
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-05-09 16:59:13 -0700

    updated assignment

--- js/dates.js
+++ js/dates.js
@@ -1,5 +1,11 @@
 //STEP 1
 
+
+
+console.log();
+
+
+
 //STEP 2
 
 //STEP 3
@@ -7,3 +13,13 @@
 //STEP 4
 
 //STEP 5
+
+
+
+
+
+// Create an application that gets the number of days in a month. Display that result within the console window.
+// Create an application that gets the month name from a particular date. Display that result within the console window.
+// Create an application that tests whether a date is a weekend. Display that result within the console window.
+// Create an application that gets yesterday’s day of the week. For instance, if today is Tuesday, the console window should display Monday.
+// Create an application that gets the current day of the week. The twist here is that I want only the first letter of the day. If today is Tuesday, the letter T should be displayed in the console window.
