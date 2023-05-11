3f42ad0f7f4205dfa46e0d2adf58d3dd51a036b2
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-05-10 19:37:48 -0700

    assignment completed

--- js/math.js
+++ js/math.js
@@ -1,19 +1,26 @@
 //STEP 1
 
+
 // let userNumber = prompt('Choose a number.');
 // console.log(Math.abs(userNumber));
 
+
 //STEP 2
+
 
 // let userFloat = prompt('Choose a floating point value (decimal).');
 // console.log(Math.ceil(userFloat));
 
+
 //STEP 3
+
 
 // let userFloat = prompt('Choose a floating point value (decimal).');
 // console.log(Math.floor(userFloat));
 
+
 //STEP 4
+
 
 // let userNumbers = prompt('Choose five numbers and separate them with a comma. No spaces.');
 // let newNumbers = userNumbers.split(',');
@@ -22,7 +29,9 @@
 // console.log(Math.max(...newNumbers));
 // console.log(Math.min(...newNumbers));
 
+
 //STEP 5
+
 
 // let baseNumber = prompt('Choose a number.');
 // let squaredNumber = Math.sqrt(baseNumber);
--- js/strings.js
+++ js/strings.js
@@ -1,38 +1,51 @@
 //STEP 1
 
+
 // let userName = prompt('What is your name?').length;
 // alert(userName);
 
+
 //STEP 2
+
 
 // let userName = prompt(`What is your name?`);
 // let totalChar = userName.length - 1;
 // let charChoice = prompt(`Pick a number between 0 and ${totalChar}.`);
 // alert(userName.charAt(charChoice));
 
+
 //STEP 3
+
 
 // let firstName = prompt('What is your FIRST name?') + ' ';
 // let lastName = prompt('What is your LAST name?');
 // alert(`Your name is: ${firstName.concat(lastName)}.`);
 
+
 //STEP 4
+
 
 // let story = 'The quick brown fox jumps over the lazy dog';
 // alert(story.indexOf('fox'));
 
+
 //STEP 5
+
 
 // let story = 'The quick brown fox jumps over the lazy fox';
 // alert(story.lastIndexOf('fox'));
 
+
 //STEP 6
+
 
 // let story = 'The quick brown fox jumped over the lazy dog';
 // let fullName = prompt('What is your FULL NAME?');
 // alert(story.replace('the lazy dog', fullName));
 
+
 //STEP 7
+
 
 // let story = 'The quick brown fox jumps over the lazy dog';
 // let word = prompt('Choose a word.');
@@ -50,16 +63,21 @@
 
 //STEP 8
 
+
 // let old_string = 'The quick brown fox jumps over the lazy dog';
 // let new_string = old_string.slice(31);
 // alert(new_string.toUpperCase());
 
+
 //STEP 9
+
 
 // let story = ' THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ';
 // alert(story.trim().toLowerCase())      
 
+
 //STEP 10
+
 
 // let story = 'the quick brown fox jumps over the lazy dog';
 // let capitalThe = story.slice(0,1).toUpperCase();
